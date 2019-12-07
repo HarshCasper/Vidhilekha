ddb7df402649c038233a8df52ec06f8a0f7c8cbe
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-07 20:48:21 +0530

    Create script.js

---
+++ Calculator/script.js
@@ -0,0 +1,12 @@
+function fun(x)
+{
+    document.getElementById('input').value = `${document.getElementById('input').value}${x}` ;
+}
+function func()
+{
+    document.getElementById('input').value=eval(document.getElementById('input').value);
+}
+function c()
+{
+    document.getElementById('input').value ="";
+}
